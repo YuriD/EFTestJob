f96502ae98e72d4c71e36a52635b16d35b9e3707
Yuriy YD <yurida@mail.ru>
2022-01-13 21:29:58 +0700

    change calculate & userslist

--- ClientApp/src/components/Calculate.js
+++ ClientApp/src/components/Calculate.js
@@ -5,7 +5,12 @@ import { UserContext } from './UserContext'
 export const UsersPlot = () => {
   const u = useContext(UserContext)
   const data = {}
-  let ret7d = 0, reg7d = 0, now = new Date(), diffTime = 0, diffDay = 0, rollRet = 0 
+  let ret7d = 0, reg7d = 0, now = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()),
+      diffTime = 0, diffDay = 0, rollRet = 0
+
+  if (u.users.length === 0) {
+    return <p className='p_calc'>There is no data available</p>
+  }
 
   u.users.forEach(user => {
     diffTime = Math.abs(new Date(user.dLastAct) - new Date(user.dReg))
@@ -22,13 +27,18 @@ export const UsersPlot = () => {
   rollRet = (ret7d / reg7d * 100).toPrecision(3)
 
   return <>
-    <p style={{ textAlign: 'center', padding: '2rem', fontSize: '15pt', fontFamily: 'verdana', color: 'blue' }}>
-      Rolling Retention 7 day: <span style={{ color: 'Brown' }}>{rollRet}%</span> </p>
+    <p className='p_calc'>Rolling Retention 7 day: <span style={{ color: 'Brown' }}>{reg7d !== 0?rollRet+'%':'no valid data'}</span> </p>
     <Plot
       data={[
-        { type: 'bar', x: Object.keys(data), y: Object.keys(data).map(key => data[key]), marker: { color: 'darkseagreen'  }}
+        { type: 'bar', x: Object.keys(data), y: Object.keys(data).map(key => data[key]), marker: { color:'SeaGreen'  }}
       ]}
-      layout={{ title: "Distribution of user lives in days", font: { color: 'blue', size: 14 } }}
+      layout={{
+        title: "Distribution of user lives in days",
+        font: { color: 'blue', size: 14 },
+        xaxis: {
+          title: 'days from registration date to date of last activity', titlefont: { size: 18, color: 'Sienna' }},
+        yaxis: { title: 'number of users', titlefont: { size: 18, color: 'Sienna' }}
+      }}
       style={{ width: '100%', height: 650 }}
     />
   </>;
